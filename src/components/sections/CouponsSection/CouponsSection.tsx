282cebde22cfc278fc3bc1408086098d90a2983b
VietLinh1104 <linhtx2004@gmail.com>
2025-08-02 13:54:55 +0700

    .next

--- src/components/sections/CouponsSection/CouponsSection.tsx
+++ src/components/sections/CouponsSection/CouponsSection.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
-import type { Coupon } from "@/types";
+import type { Coupon } from "@/types/Types";
 
 export const CouponsSection = (): JSX.Element => {
   const coupons: Coupon[] = [
